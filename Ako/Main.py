051d3bafb9b647367634857b420815aba658108e
22074-del <22074@nayland.school.nz>
2026-05-18 10:49:49 +1200

    third commit

--- Ako/Main.py
+++ Ako/Main.py
@@ -1,1 +1,27 @@
-print("hello")
+quiz_questions = [
+    {
+        "prompt" : "what does 'Mahi' translate to?",
+        "options" : ["Work", "Car", "Fish", "School"]
+        "answer" : "Work"
+    },
+    {
+        "prompt": "What does 'Whānau' translate to?",
+        "options": ["Clothing", "Bird", "Family", "Food"],
+        "answer": "Family"
+    }
+    {
+        "prompt": "What does 'Aroha' translate to?",
+        "options": ["Love", "Hate", "Friendship", "Money"],
+        "answer": "Love"
+    }
+    {
+        "prompt": "What does 'Kia ora' translate to?",
+        "options": ["Goodbye", "Hello", "Thank you", "Please"],
+        "answer": "Hello"
+    }
+    {
+        "prompt": "What does 'Mana' translate to?",
+        "options": ["Prestige", "Water", "Food", "Clothing"],
+        "answer": "Prestige"
+    }
+]
